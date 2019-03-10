c4436ea4f6c3ae6174ec36d0aa4613de1045779e
justbegginer <46086383+justbegginer@users.noreply.github.com>
2019-03-10 16:38:08 +0300

    v.0.1.1
    now my snake(if it can be called snake) can eat apples.

--- zmeyka/zmeyka/Form1.cs
+++ zmeyka/zmeyka/Form1.cs
@@ -13,9 +13,14 @@ namespace zmeyka
     public partial class Form1 : Form
     {
         int xCordinate = 0;
+        int snakeWidth = 50;
+        int snakeHeigth = 50;
         int yCordinate = 0;
         int firstAppleX = 0;
         int firstAppleY = 0;
+        int score = 0;
+        List<int> tailXCoordinate = new List<int>();
+        List<int> tailYCoordinate = new List<int>(); 
         public Form1()
         {
             InitializeComponent();
@@ -35,21 +40,30 @@ namespace zmeyka
                 yCordinate = secondNumber;
             }
             Graphics canvas = e.Graphics;
-            canvas.FillEllipse(Brushes.LimeGreen, new RectangleF(xCordinate, yCordinate, 50 , 50));
+            canvas.FillEllipse(Brushes.LimeGreen, new RectangleF(xCordinate, yCordinate, snakeWidth , snakeHeigth));
+            GenerateApples();
+            Graphics apple = e.Graphics;
+            apple.FillEllipse(Brushes.BlueViolet, new RectangleF(firstAppleX,firstAppleY,10,10));
+        }
+        private void ArrayOfSnakeDetails()
+        {
+
+        }
+        private void GenerateApples()
+        {
+            text.Text=Convert.ToString(score);
+            Random random = new Random();
             int firstAppleXRandom = random.Next(10, GameArea.Width - 10);
-            if (firstAppleX==0)
+            if (firstAppleX == 0)
             {
                 firstAppleX = firstAppleXRandom;
             }
             int firstAppleYRandom = random.Next(10, GameArea.Height - 10);
-            if (firstAppleY==0)
+            if (firstAppleY == 0)
             {
                 firstAppleY = firstAppleYRandom;
             }
-            Graphics apple = e.Graphics;
-            apple.FillEllipse(Brushes.BlueViolet, new RectangleF(firstAppleX,firstAppleY,10,10));
         }
-
         private void KeyDown0(object sender, KeyEventArgs e)
         {
             
@@ -73,9 +87,44 @@ namespace zmeyka
             GameArea.Invalidate();
             if (xCordinate < 0 | xCordinate>GameArea.Width-50 | yCordinate<0 | yCordinate>GameArea.Height-50)
             {
-                MessageBox.Show("ты сломался");
+                MessageBox.Show("ты сломался ");
+
                 //GameArea.Invalidate();
             }
+            ApplesEating();
+        }
+        private void ApplesEating()
+        {
+            if (new Rectangle(xCordinate,yCordinate,50,50).IntersectsWith(new Rectangle(firstAppleX,firstAppleY,10,10)))
+            {
+
+                MessageBox.Show("Ням");
+                score++;
+                firstAppleX = 0;
+                firstAppleY = 0;
+                GenerateApples();
+                GameArea.Invalidate();
+            }
+        }
+        private void GenerateRandomForTail()
+        {
+            Random random = new Random();
+            int computerChoice = random.Next(0,3);
+            switch(computerChoice)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
+        private void GenerateTail(int xCordinateForTail , int yCordinateForTail )
+        {
+            
         }
     }
 }
